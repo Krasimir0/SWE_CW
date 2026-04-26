e94bcf7654d64b805126d26ca0bc87350d47e8a0
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2026-04-26 19:26:26 +0100

    Add Refund class

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Refund.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author krasipetranov
+ */
+public class Refund {
+    private int refundId;
+    private double amount;
+    private String reason;
+}
